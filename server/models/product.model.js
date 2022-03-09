765119a16d243ad435a795335835f7f33fabec89
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-08 20:28:16 -0800

    added functionality to form

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -3,6 +3,5 @@ const ProductSchema = new mongoose.Schema({
     title: {type: String},
     price: {type: Number},
     description: {type: String}
-    
 }, {timestamps: true });
 module.exports= mongoose.model('Product', ProductSchema);
