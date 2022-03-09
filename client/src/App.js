0fb886cc8b0f034ed3b96599fee746cf1e330e20
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-08 21:15:03 -0800

    Added Bootstrap for quick styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import  {useEffect, useState} from 'react'
+
 import ProductForm from './components/ProductForm';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
+
     <div className="App">
       <ProductForm/>
     </div>
--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from 'react'
 import axios from 'axios'
 
+
 const ProductForm = () => {
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState(0);
@@ -25,13 +26,21 @@ const ProductForm = () => {
 
     return(
         <form onSubmit={submitHandler}>
-            <label>Title:</label>
-            <input type="text" onChange={(e) => setTitle(e.target.value)} /><br/>
-            <label>Price:</label>
-            <input type="number" onChange={(e) => setPrice(e.target.value)}/><br/>
-            <label>Description:</label>
-            <input type="text" onChange={(e) => setDescription(e.target.value)}/><br/>
-            <input type="submit" value="Submit" />
+            <div className="form-group">
+                <label>Title:</label>
+                <input className="form-control" type="text" onChange={(e) => setTitle(e.target.value)} />
+            
+            <div className="form-group"></div>
+                <label>Price:</label>
+                <input className="form-control" type="number" onChange={(e) => setPrice(e.target.value)}/>
+            </div>
+            <div className="form-group">
+                <label >Description:</label>
+                <input className="form-control" type="text" onChange={(e) => setDescription(e.target.value)}/>
+            </div>
+            <div className="form-group">
+                <input class="btn btn-primary" type="submit" value="Submit" />
+            </div>
         </form>
     )
 }
